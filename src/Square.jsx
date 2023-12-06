b5f9e2df276cb6174244d7f184b58f40ded29ef0
Jerkner <philip@jerkner.se>
2023-12-06 15:56:07 +0000

    Revert "revert"
    This reverts commit 1532505ebde5c19544335885953ede9dd258566c.

--- src/Square.jsx
+++ src/Square.jsx
@@ -6,7 +6,7 @@ console.log(X, O)
 
 export default function Square({ sign, onClick, gameOn }) {
     const convertedSign =
-        sign === 1 ? X : sign === -1 ? O : "public/assets/null.png"
+        sign === 1 ? X : sign === -1 ? O : "src/assets/null.png"
 
     const classList = gameOn && !sign ? "square hover" : "square"
 
